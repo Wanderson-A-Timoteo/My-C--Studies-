248b87ef24e5a8511137a60a0504380823f735e8
Wanderson-A-Timoteo <wanderson_timoteo@hotmail.com>
2020-10-08 09:34:25 -0400

    add aula03 exemplo de variaveis

---
+++ Aula03/Aula03.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Aula03 {
+  static void Main(){
+    byte n1 = 10; // 0 e 255
+    int num = 0;
+    char letra = 'c'; // Apenas uma letra
+    float valor = 5.3f;
+    string nome = "Wanderson"; // Varias letras
+
+    var aux = nome; // 
+
+    Console.WriteLine(aux);
+  }
+}

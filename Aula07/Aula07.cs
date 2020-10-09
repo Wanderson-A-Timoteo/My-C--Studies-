a3e5e164a791910903a75be26e3e467b0fb20624
Wanderson-A-Timoteo <wanderson_timoteo@hotmail.com>
2020-10-09 08:40:22 -0400

    add Aula07.cs  como declarar constantes e atribuir o valor inicial a constante

---
+++ Aula07/Aula07.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Aula07 {
+
+  static void Main() {
+    const string canal = "CFB Cursos"; // Constantes não podem ter seus valores mudados
+    const double pi = 3.1415;
+
+
+    Console.WriteLine("Canal {0} \n Pi: {1}", canal, pi);
+  }
+}

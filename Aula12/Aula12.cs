580a1d53c94d6f80fc0168aa7d465d06bba997ea
Wanderson-A-Timoteo <wanderson_timoteo@hotmail.com>
2020-10-13 09:23:04 -0400

    add Aula12.cs comando condicional IF.

---
+++ Aula12/Aula12.cs
@@ -0,0 +1,35 @@
+using System;
+
+class Aula12{
+
+  static void Main(){
+    
+    int n1, n2, n3, n4, res, soma;
+    soma=res=n1=n2=n3=n4=0;
+
+    string resultado="Reprovado";
+
+    Console.Write("Digite a nota 1: ");
+    n1=int.Parse(Console.ReadLine());
+
+    Console.Write("Digite a nota 2: ");
+    n2 = int.Parse(Console.ReadLine());
+
+    Console.Write("Digite a nota 3: ");
+    n3 = int.Parse(Console.ReadLine());
+
+    Console.Write("Digite a nota 4: ");
+    n4 = int.Parse(Console.ReadLine());
+
+    soma = n1 + n2 + n3 + n4;
+    res = soma/4;
+
+
+    if(res >= 60){
+      resultado="Aprovado";
+
+    }
+
+    Console.WriteLine("\n Nota média: {0} \n Resultado: {1}", res, resultado);
+  }
+}

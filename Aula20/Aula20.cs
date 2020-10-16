83d6138dbc2b24eed395eaa2cd8977cc8e9af5ff
Wanderson-A-Timoteo <wanderson_timoteo@hotmail.com>
2020-10-16 08:51:20 -0400

    add Aula20.cs loop WHILE

---
+++ Aula20/Aula20.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Aula20 {
+
+  static void Main(){
+
+    int[] num = new int[10];
+    int i=0;
+
+    while(i < num.Length) {
+      
+      num[i]=0;
+      
+      Console.WriteLine(num[i]);
+      
+      i++;
+    }
+    Console.WriteLine("Fim do loop");
+  }
+}
